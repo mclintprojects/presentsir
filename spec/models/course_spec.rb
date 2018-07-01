3b2a9466056c2ef62c547ae6c09e5c661ea0bdc7
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-01 17:53:06 +0000

    added teacher course associations

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Course, type: :model do
   it {should validate_presence_of :name}
   it {should validate_presence_of :identifier}
+  it {should belong_to :teacher}
 end
