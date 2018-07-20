49b536936f0ad0cc606bc100a330864781620675
Mbah Clinton <clintonmbah44@gmail.com>
2018-07-20 19:35:36 +0000

    removed bad migration file

--- db/migrate/20180719230048_add_approval_index.rb
+++
@@ -1,5 +0,0 @@
-class AddApprovalIndex < ActiveRecord::Migration[5.2]
-  def change
-    add_index :attendances, :approval
-  end
-end
--- db/migrate/20180719230259_add_approved_index.rb
+++ db/migrate/20180719230259_add_approved_index.rb
@@ -1,6 +1,5 @@
 class AddApprovedIndex < ActiveRecord::Migration[5.2]
   def change
     add_index :attendances, :approved
-    remove_index :attendances, name: 'index_attendances_on_approval'
   end
 end
